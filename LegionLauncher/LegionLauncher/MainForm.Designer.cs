8f834cf043ee736f1f0008bbffc1d37abb76c4c5
Dalton Stewart <djs856@utulsa.edu>
2015-12-29 21:58:53 -0600

    Start of the Legion ARMA Launcher. A simple form with two tabs for the server browser and the mod control. Also, I used the favicon from the website for the form icon and also used a Legion image for the background of the tabs.

---
+++ LegionLauncher/LegionLauncher/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LegionLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainTabControl = new System.Windows.Forms.TabControl();
+            this.servers = new System.Windows.Forms.TabPage();
+            this.mods = new System.Windows.Forms.TabPage();
+            this.mainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Controls.Add(this.servers);
+            this.mainTabControl.Controls.Add(this.mods);
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(521, 547);
+            this.mainTabControl.TabIndex = 0;
+            // 
+            // servers
+            // 
+            this.servers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("servers.BackgroundImage")));
+            this.servers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.servers.Location = new System.Drawing.Point(4, 25);
+            this.servers.Name = "servers";
+            this.servers.Padding = new System.Windows.Forms.Padding(3);
+            this.servers.Size = new System.Drawing.Size(513, 518);
+            this.servers.TabIndex = 0;
+            this.servers.Text = "Legion Servers";
+            this.servers.UseVisualStyleBackColor = true;
+            // 
+            // mods
+            // 
+            this.mods.BackColor = System.Drawing.Color.Transparent;
+            this.mods.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mods.BackgroundImage")));
+            this.mods.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.mods.Location = new System.Drawing.Point(4, 25);
+            this.mods.Name = "mods";
+            this.mods.Padding = new System.Windows.Forms.Padding(3);
+            this.mods.Size = new System.Drawing.Size(513, 518);
+            this.mods.TabIndex = 1;
+            this.mods.Text = "Mods";
+            this.mods.Click += new System.EventHandler(this.mods_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(521, 547);
+            this.Controls.Add(this.mainTabControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Legion ARMA Launcher";
+            this.mainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage servers;
+        private System.Windows.Forms.TabControl mainTabControl;
+        private System.Windows.Forms.TabPage mods;
+    }
+}
+
